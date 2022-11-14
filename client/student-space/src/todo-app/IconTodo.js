f27c49d4cc526ecd15a5e9568156bb4732c83dc2
John Aldrine Ilao <codingwithaldrine@gmail.com>
2022-11-14 11:12:26 +0800

    Updated IconTodo.js, Todo.js

--- client/student-space/src/todo-app/IconTodo.js
+++ client/student-space/src/todo-app/IconTodo.js
@@ -1,30 +1,23 @@
-<<<<<<< HEAD
-import React from "react";
+import React, { useState } from 'react';
+import Button from '../components/Button';
+import Todo from './Todo';
 
-export default function IconTodo(){
-    return(
-        <div>Todo</div>
-=======
-import React, { useState } from "react";
-import Button from "../components/Button";
-import Todo from "./Todo";
-
-export default function IconTodo(){
-    const [isClicked, setIsClicked] = useState(false);
-    const Content = () =>{
-        if(isClicked){
-            return(
-                <Todo/>
-            )
-        }else{
-            return(
-                <Button name="Todo" onClick={()=>{setIsClicked(true)}}/>
-            )
-        }
+export default function IconTodo() {
+  const [isClicked, setIsClicked] = useState(false);
+  const Content = () => {
+    if (isClicked) {
+      return <Todo />;
+    } else {
+      return (
+        <Button
+          name='Todo'
+          onClick={() => {
+            setIsClicked(true);
+          }}
+        />
+      );
     }
+  };
 
-    return(
-        <Content/>
->>>>>>> cdff5490b50c1b132784c515dc19dd67ad3061f8
-    )
+  return <Content />;
 }
--- client/student-space/src/todo-app/Todo.js
+++ client/student-space/src/todo-app/Todo.js
@@ -1,24 +1,16 @@
-<<<<<<< HEAD
-import React from "react";
+import React, { Component } from 'react';
+import Button from '../components/Button';
+import Input from '../components/Input';
+import './Todo.css';
 
-export default function Todo(){
-    return(
-        <div>Todo</div>
-=======
-import React,{Component} from "react";
-import Button from "../components/Button";
-import Input from "../components/Input";
-import "./Todo.css";
-
-export default function Todo(){
-    return(
-        <div className="todo-wrapper">
-            <div>
-                <Input placeHolder="todo here ..."/>
-                <Button name="Add"/>
-            </div>
-            <div></div>
-        </div>
->>>>>>> cdff5490b50c1b132784c515dc19dd67ad3061f8
-    )
+export default function Todo() {
+  return (
+    <div className='todo-wrapper'>
+      <div>
+        <Input placeHolder='todo here ...' />
+        <Button name='Add' />
+      </div>
+      <div></div>
+    </div>
+  );
 }
