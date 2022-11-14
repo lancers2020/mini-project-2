a0816122c389682734f7443ea4249c00c65422b6
John Aldrine Ilao <codingwithaldrine@gmail.com>
2022-11-14 11:10:15 +0800

    Updated Home.js

--- client/student-space/src/pages/Home.js
+++ client/student-space/src/pages/Home.js
@@ -4,13 +4,9 @@ import IconTodo from '../todo-app/IconTodo';
 
 function Home() {
   return (
-<<<<<<< HEAD
-    <div>
-=======
-    <div style={{display: "flex", justifyContent: "center"}}>
->>>>>>> cdff5490b50c1b132784c515dc19dd67ad3061f8
-      <IconTodo/>
-      <IconCal/>
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <IconTodo />
+      <IconCal />
     </div>
   );
 }
