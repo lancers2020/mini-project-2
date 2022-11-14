d134265635ccd852435e66a15b4279f00f3e9ea3
davelimutin <dave.limutin.pixel8@gmail.com>
2022-11-15 06:06:52 +0800

    created a layout for grades page at Grades.js

--- client/student-space/src/pages/Grades.js
+++ client/student-space/src/pages/Grades.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React,{Component} from 'react';
+import "./Pages.css";
+
+function Layout(){
+  return(
+    <div className='layout-container'>
+      <div>
+        <div>
+          <div>photo</div>
+          <div>
+            <div>name</div>
+            <div>class</div>
+          </div>
+        </div>
+        <div>
+          <div>select</div>
+          <div>submit</div>
+        </div>
+      </div>
+      <div>
+        grades content
+      </div>
+    </div>
+  )
+}
 
 function Grades() {
   return (
-    <div>GRADES Page</div>
+    <div className='grades-container'>
+      <Layout/>
+    </div>
   )
 }
 
