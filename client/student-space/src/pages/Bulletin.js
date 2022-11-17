e3e187a550c2b581db26d7ec656a39011fe9a3c0
davelimutin <dave.limutin.pixel8@gmail.com>
2022-11-18 03:30:28 +0800

    Bulletin.js done

--- client/student-space/src/pages/Bulletin.js
+++ client/student-space/src/pages/Bulletin.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import FireworkLeft from "../pictures/fireworks.png";
 import FireworkRight from "../pictures/fireworks_2.png";
+import Card from "../components/Card";
+import CodingFirstPlace from "../pictures/pinoy-competition.jpg";
 
 function Bulletin() {
+  const data = [
+    "DCNHS winning the NATIONAL-level Coding Competition!",
+    "DCNHS scores again! Top 4 in the Regional Extemporaneous Speaking held in Baguio City.",
+    "Fun collaboration with CCNHS #UNITY",
+    "Enjoy and spend time with your Families this Christmas! See you on January 4th, 2023!"
+  ]
   return (
     <div className='bulletin-container'>
       <div className='bulletin-left'>
@@ -21,8 +29,27 @@ function Bulletin() {
         <div id="fireworks-right" className='fireworks'><img src={FireworkRight} width="100%" heigh="auto"/></div>
         <h5>Classes will continue on January 4, 2023!</h5>
       </div>
-      <div className='bulletin-right'>
 
+      <div className='events'>EVENTS</div>
+
+      <div className='bulletin-right'>
+        {data.map((item)=>{
+          return(
+            <Card padding="10px" content={
+              <div className='status-container'>
+                <div>
+                  <div className='status-profile'>
+                    <div>DCNHS</div>
+                    <div>5hrs ago</div>
+                  </div>
+                  <div>{item}</div>
+                </div>
+                <div><img src={CodingFirstPlace} width="100%" height="auto" alt="coding-competition"/></div>
+              </div>
+            }/>
+          )
+        })}
+        <div className='post-container'></div>
       </div>
     </div>
   )
