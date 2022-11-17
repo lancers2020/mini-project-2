05620beba4aed655f4b451a8c1ad5912f009876b
davelimutin <dave.limutin.pixel8@gmail.com>
2022-11-17 16:33:51 +0800

    Partial features added on Bulletin.js page

--- client/student-space/src/pages/Bulletin.js
+++ client/student-space/src/pages/Bulletin.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 function Bulletin() {
   return (
-    <div>BULLETIN Page</div>
+    <div className='bulletin-container'>
+      <div className='bulletin-left'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <h2 data-text="2022-2023">
+          2022-2023
+        </h2>
+        <h3>
+          2022 was a rainbow of POSSIBILITIES, so here's to creating NEW GOALS for 2023
+        </h3>
+        <div id="quotations-start" className='quotations'></div>
+        <div id="quotations-end" className='quotations'></div>
+      </div>
+      <div className='bulletin-right'>
+
+      </div>
+    </div>
   )
 }
 
