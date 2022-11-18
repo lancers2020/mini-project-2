d4a785ffc31fcfcfa243d9992657ad66eef16806
John Aldrine Ilao <codingwithaldrine@gmail.com>
2022-11-19 00:49:21 +0800

    changed css button

--- client/student-space/src/stopwatch/Stopwatch.js
+++ client/student-space/src/stopwatch/Stopwatch.js
@@ -30,14 +30,20 @@ const Stopwatch = () => {
 
       <div id='buttons'>
         {!timerOn && time === 0 && (
-          <button onClick={() => setTimerOn(true)}>Start</button>
+          <button className='stopwatch-button' onClick={() => setTimerOn(true)}>
+            Start
+          </button>
         )}
         {timerOn && <button onClick={() => setTimerOn(false)}>Stop</button>}
         {!timerOn && time > 0 && (
-          <button onClick={() => setTime(0)}>Reset</button>
+          <button className='stopwatch-button' onClick={() => setTime(0)}>
+            Reset
+          </button>
         )}
         {!timerOn && time > 0 && (
-          <button onClick={() => setTimerOn(true)}>Resume</button>
+          <button className='stopwatch-button' onClick={() => setTimerOn(true)}>
+            Resume
+          </button>
         )}
       </div>
     </div>
