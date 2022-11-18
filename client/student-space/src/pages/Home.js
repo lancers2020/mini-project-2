b41a9a2b34c71d61864ed70410122fb09e06ef07
John Aldrine Ilao <codingwithaldrine@gmail.com>
2022-11-18 22:36:22 +0800

    added stopwatch features

--- client/student-space/src/pages/Home.js
+++ client/student-space/src/pages/Home.js
@@ -1,19 +1,20 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import IconCalculator from '../calculator/IconCalculator';
 import Clock from '../components/Clock';
 import IconTodo from '../todo-app/IconTodo';
-import "./Pages.css";
+import './Pages.css';
+import Stopwatch from '../stopwatch/Stopwatch';
 
 function Home() {
   return (
     <div className='home-container'>
       <div className='home-left'>
-        <Clock/>
+        <Clock />
       </div>
-      <div style={{marginInline: "10px"}}>
+      <div style={{ marginInline: '10px' }}>
         <IconTodo />
       </div>
-      <div style={{marginInline: "10px"}}>
+      <div style={{ marginInline: '10px' }}>
         <IconCalculator />
       </div>
     </div>
